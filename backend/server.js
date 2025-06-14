55c298cd6f739ae96d2958392e2513e80fa25952
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2025-06-14 18:56:52 +0530

    project compleated

---
+++ backend/server.js
@@ -0,0 +1,101 @@
+import express from 'express';
+import helmet, { contentSecurityPolicy } from 'helmet';    
+import morgan from 'morgan';    
+import cors from 'cors'; 
+import dotenv from 'dotenv';
+import path from "path";
+
+import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
+import productRoutes from './routes/productRoutes.js';
+
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Default to port 3000 if not specified
+const __dirname = path.resolve();
+
+
+ 
+app.use(express.json()); // Parse JSON bodies
+app.use(cors());
+app.use(helmet({
+  contentSecurityPolicy: false, }// Disable CSP for simplicity, adjust as needed
+)); // Security middleware
+
+app.use(morgan('dev')); // Logg the requests
+
+
+
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    // check for spoofed bots
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
+
+
+
+app.use('/api/products',productRoutes); // Use product routes
+
+if (process.env.NODE_ENV === "production") {
+  // server our react app
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+ app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
+
+});
+
